6bf465d2bce1959d40d70cb2af1715c93e062136
gagan7git <mysoregraphics@gmail.com>
2022-08-18 03:54:06 -0500

    initial commit

--- create_splits.py
+++ create_splits.py
@@ -6,6 +6,7 @@ import random
 import numpy as np
 
 from utils import get_module_logger
+import shutil
 
 
 def split(data_dir):
@@ -14,11 +15,37 @@ def split(data_dir):
     same directory. This folder should be named train, val and test.
 
     args:
-        - data_dir [str]: data directory, /home/workspace/data/waymo
+        - data_dir [str]: data directory, /home/workspace/data
     """
     
     # TODO: Split the data present in `/home/workspace/data/waymo/training_and_validation` into train and val sets.
+    try:
+        files = [filename for filename in glob.glob(f'{data_dir}/waymo/training_and_validation/*.tfrecord')]
+    except:
+        print("Issue in TF record file parse")
+    np.random.shuffle(files)	
+    train_files, val_file = np.split(files, [int(.75*len(files))])
     # You should move the files rather than copy because of space limitations in the workspace.
+	
+    train = os.path.join(data_dir, 'train')
+    val = os.path.join(data_dir, 'val') 
+
+    try:	
+        if os.path.exists(train):
+            os.makedirs(train)
+        os.makedirs(train,exist_ok=True)   
+        
+        if os.path.exists(val):
+            os.makedirs(val)
+        os.makedirs(val,exist_ok=True) 
+    except:
+        print("pass")	
+
+	
+    for file in train_files:
+        shutil.move(file, train)   
+    for file in val_file:
+        shutil.move(file, val)	
 
 if __name__ == "__main__": 
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
